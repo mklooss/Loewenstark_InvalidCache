f2f6cb362f927ac2f0bc108ab425797b3340c3db
Mathis Klooss <mathis@klooss.net>
2012-10-10 18:47:09 +0200

    initial release

---
+++ app/code/local/Loewenstark/InvalidCache/Model/Observer.php
@@ -0,0 +1,34 @@
+<?php
+
+class Loewenstark_InvalidCache_Model_Observer
+{
+    /**
+     * Remove the annoying notification on product save.
+     * @see https://gist.github.com/1074482
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function clearBlockCache(Varien_Event_Observer $observer)
+    {
+        Mage::app()->getCacheInstance()->cleanType('block_html');
+        return $this;
+    }
+    
+    /**
+     * Clean Up Cache via CronJob
+     *
+     * @return void
+     */
+    public function clearInvalidCache()
+    {
+        // get all Cache Types
+        $types = Mage::app()->getCacheInstance()->getInvalidatedTypes();
+        foreach($types as $_type)
+        {
+            // clear invalid caches
+            Mage::app()->getCacheInstance()->cleanType($_type->getId());
+            Mage::Log(sprintf("Cleared Cache: %s",$_type->getId()),null);
+        }
+        return $this;
+    }
+}
